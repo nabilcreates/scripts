d4432e378dc0ac71c28467f73bf68a4ee2e0283d
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-11-07 15:53:23 +0800

    Changed folder structure

--- required/i3/run.sh
+++ required/i3/run.sh
@@ -4,4 +4,4 @@
 sudo apt update
 
 # Install basic i3 with nitrogen and dmenu
-sudo apt install i3 i3status dmenu nitrogen lxappearance -y
+sudo apt install i3-wm i3status dmenu nitrogen lxappearance -y
