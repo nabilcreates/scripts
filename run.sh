f552d4897f83d05eee4da5d6e59abf6394fc4511
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-11-07 15:53:04 +0800

    Modified script file

--- run.sh
+++ run.sh
@@ -4,11 +4,13 @@ echo "Install Menu"
 echo "======================================"
 echo "Enter 0 to install i3"
 echo "Enter 1 to install i3-gaps"
+echo "Enter 2 to see what packages it installs"
 echo "======================================"
 
 echo "Selection:"
 read answer
 case "$answer" in
-0) sudo bash ./sh/i3/run.sh;;
-1) sudo bash ./sh/i3-gaps/install.sh;;
+0) sudo bash ./required/i3/run.sh;;
+1) sudo bash ./required/i3-gaps/install.sh;;
+2) echo 'i3-wm i3status dmenu nitrogen lxappearance';;
 esac
